5e4ce3ddfabb59eed61775d3c8ff77be05c95fc2
Rafael Pereira <rafaelspereira1@hotmail.com>
2022-03-25 20:19:18 -0300

    Create lead-service.js

---
+++ src/services/lead-service.js
@@ -0,0 +1,34 @@
+import { v4 } from "uuid";
+
+export function createLead(lead) {
+  const rawLeads = localStorage.getItem("leads");
+
+  const leads = JSON.parse(rawLeads) || [];
+
+  const newLeads = [
+    ...leads,
+    {
+      id: v4(),
+      index: leads.length,
+      ...lead,
+    },
+  ];
+
+  localStorage.setItem("leads", JSON.stringify(newLeads));
+}
+
+export function getLeads() {
+  const rawLeads = localStorage.getItem("leads");
+
+  return JSON.parse(rawLeads) || [];
+}
+
+export function updateLead(newLead) {
+  const leads = getLeads();
+
+  const newLeads = leads.map((lead) =>
+    lead.id === newLead.id ? newLead : lead
+  );
+
+  localStorage.setItem("leads", JSON.stringify(newLeads));
+}
